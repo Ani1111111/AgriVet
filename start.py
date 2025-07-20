2babf98d949b39a3ab75a4a13ae9ae045d2f437a
dc_animesh <animesh.verma@datachannel.co>
2025-07-20 12:59:18 +0530

    added req git ignore

--- start.py
+++ start.py
@@ -1,1 +1,14 @@
-print('adsfasf')
+from flask import Flask
+
+# Create the Flask application instance
+app = Flask(__name__)
+
+# Home route
+@app.route('/')
+def home():
+    return 'Hello! Welcome to the home page.'
+
+
+# Run the app
+if __name__ == '__main__':
+    app.run(debug=True)
